0f4f0ec178e8386ee97e6b5c3e08a8037523c366
walter.laurito <walter.laurito@volunteerworld.com>
2021-01-27 20:41:16 +0100

    add aircraft examples

--- core/aircraft.py
+++ core/aircraft.py
@@ -13,8 +13,3 @@ class Aircraft(collections.namedtuple('Aircraft', ['jsbsim_id', 'flightgear_id',
 
     def get_cruise_speed_fps(self) -> float:
         return self.cruise_speed_kts * self.KTS_TO_FT_PER_S
-
-
-cessna172P = Aircraft('c172p', 'c172p', 'Cessna172P', 120)
-f15 = Aircraft('f15', 'f15c', 'F15', 220)   # observed speed at engine settings used alt = 5k ft
-a320 = Aircraft('A320', 'A320-211', 'A320', 490)
